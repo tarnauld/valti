e98798b8d00af959a54cb3e0c6d54ed227c9ccf2
Valentin Fries <contact@fries.io>
2016-06-14 10:57:36 +0200

    Ajout du flag GCC -Wall dans le build.bat

--- bin/build.bat
+++ bin/build.bat
@@ -8,9 +8,9 @@ rename valti.tab.c valti.y.c
 
 flex ..\src\valti.l
 rename lex.yy.c valti.lex.c
-gcc -c valti.lex.c -o valti.lex.o
-gcc -c valti.y.c -o valti.y.o
-gcc -o valti valti.lex.o valti.y.o
+gcc -c valti.lex.c -o valti.lex.o -Wall
+gcc -c valti.y.c -o valti.y.o -Wall
+gcc -o valti valti.lex.o valti.y.o -Wall
 
 pause
 
